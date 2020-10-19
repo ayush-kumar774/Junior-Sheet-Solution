6c74d2aba24e6e7934dd2bef90a2498e718ce93c
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-10-19 14:21:45 +0530

    Junior Sheet A Codeforces Problem Solution

---
+++ A/18. 228 - A. Is your horseshoe on the other hoof?.py
@@ -0,0 +1,11 @@
+# I LOVE AAKRITI GOSWAMI
+my_set = {}
+my_set = set()
+a, b, c , d = input().split(" ")
+a , b, c ,d = int(a), int(b), int(c), int(d)
+my_set.add(a)
+my_set.add(b)
+my_set.add(c)
+my_set.add(d)
+a = len(my_set)
+print(4 - a)
