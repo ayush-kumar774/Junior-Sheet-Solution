7a4b9ed46c512356b3dfcc1de73a92e300fce607
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-11-18 19:06:03 +0530

    Codeforces Problem A Solution

--- A/1. 677 - A. Vanya and Fence.cpp
+++ A/1. 677 - A. Vanya and Fence.cpp
@@ -1,27 +1,27 @@
 //https://codeforces.com/contest/677/problem/A
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false) ;
-    cin.tie(0) ;
-    long long int n , h , count = 0 ;
-    cin >> n >> h ;
-    while(n--)
-    {
-        int x ;
-        cin >> x ;
-        if(x <= h)
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        long long int n, h, count = 0;
+        cin >> n >> h;
+        while (n--)
         {
-            count ++;
+                int x;
+                cin >> x;
+                if (x <= h)
+                {
+                        count++;
+                }
+                else if (x > h)
+                {
+                        count += 2;
+                }
         }
-        else if (x > h)
-        {
-            count+=2;
-        }
-    }
 
-    cout << count << endl;
+        cout << count << endl;
 }
 /*  CODED BY:-
  ___________________________________
